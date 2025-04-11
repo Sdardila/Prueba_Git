eb135c9c0902249287ac17c29979dfc532644717
sdardila <sdardila@SDU935-115338-L.CampusUninorte.local>
2025-04-11 10:41:20 -0500

    Fabian 2

--- pruebas_github/Pruebas_Github.java
+++ pruebas_github/Pruebas_Github.java
@@ -12,6 +12,7 @@ public class Pruebas_Github {
         System.out.println("Segundo commit desde local");
         System.out.println("Prueba de pull");
         System.out.println("Prueba Ardila");
+        System.out.println("Fabian");
     }
     
 }
